214896c1de1059b3714a6789184e5121fa12c1eb
Knoageo <162757754+KNOAGEO@users.noreply.github.com>
2024-07-10 21:14:39 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編_5章課題</title>
+</head>
+<body>
+    <p>
+    <?php
+    $score_1 = 80;
+    $score_2 = 60;
+    $score_3 = 55;
+    $score_4 = 40;
+    $score_5 = 100;
+    $score_6 = 25;
+    $score_7 = 80;
+    $score_8 = 95;
+    $score_9 = 30;
+    $score_10 = 60;
+
+$score_total = 80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60;
+echo $score_total / 10;
+
+    ?>
+    </p>
+</body>
+</html>
