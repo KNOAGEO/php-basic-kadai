470acc2de9b4a4354092befd0a60ee60cd6031ef
Knoageo <162757754+KNOAGEO@users.noreply.github.com>
2024-07-11 13:51:05 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編_7章課題</title>
+</head>
+<body>
+    <?php
+    $tamanegi_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    echo '<br>';
+    
+    print_r($tamanegi_data);
+
+
+    ?>
+    </p>
+</body>
+</html>
